19fb986137ef7af58c2fb9c1f750b8858a372a67
Max Rottenkolber <max@mr.gy>
2018-05-23 13:25:02 +0200

    lib.protocol.ipv4: fix a bug in _ulp map
    ICMP in IPv4 has protocol identifier 1, not 58.

--- src/lib/protocol/ipv4.lua
+++ src/lib/protocol/ipv4.lua
@@ -33,10 +33,10 @@ local ipv4 = subClass(header)
 ipv4._name = "ipv4"
 ipv4._ulp = {
    class_map = {
+       [1] = "lib.protocol.icmp.header",
        [6] = "lib.protocol.tcp",
       [17] = "lib.protocol.udp",
       [47] = "lib.protocol.gre",
-      [58] = "lib.protocol.icmp.header",
    },
    method    = 'protocol' }
 ipv4:init(
