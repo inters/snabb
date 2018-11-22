8ef2d114a27b55bc9260a88110feba1be16710b6
Max Rottenkolber <max@mr.gy>
2018-11-22 14:32:51 +0100

    InboundDispatch: prepend/strip ethernet header and use pf.match

--- src/program/vita/dispatch.lua
+++ src/program/vita/dispatch.lua
@@ -180,50 +180,48 @@ InboundDispatch = {
 
 function InboundDispatch:new (conf)
    local o = {
-      node_ip4n = ipv4:pton(conf.node_ip4),
-      ip4 = ipv4:new({})
+      eth4 = ethernet:new{type=0x0800},
+      p_box = ffi.new("struct packet *[1]"),
+      dispatch = pf_match.compile(([[match {
+         ip dst host %s and icmp => icmp4
+         ip dst host %s => protocol4_unreachable
+         ip => forward4
+         otherwise => reject_protocol
+      }]]):format(conf.node_ip4, conf.node_ip4))
    }
    return setmetatable(o, {__index=InboundDispatch})
 end
 
-function InboundDispatch:forward4 (p)
+function InboundDispatch:forward4 ()
+   local p = packet.shiftleft(self.p_box[0], ethernet:sizeof())
    link.transmit(self.output.forward4, p)
 end
 
-function InboundDispatch:icmp4 (p)
+function InboundDispatch:icmp4 ()
+   local p = packet.shiftleft(self.p_box[0], ethernet:sizeof())
    link.transmit(self.output.icmp4, p)
 end
 
-function InboundDispatch:protocol4_unreachable (p)
+function InboundDispatch:protocol4_unreachable ()
+   local p = packet.shiftleft(self.p_box[0], ethernet:sizeof())
    link.transmit(self.output.protocol4_unreachable, p)
 end
 
-function InboundDispatch:reject_protocol (p)
-   packet.free(p)
+function InboundDispatch:reject_protocol ()
+   packet.free(self.p_box[0])
    counter.add(self.shm.protocol_errors)
 end
 
-function InboundDispatch:dispatch (p)
-   local ip4 = self.ip4:new_from_mem(p.data, p.length)
-   if ip4 then
-      if ip4:dst_eq(self.node_ip4n) then
-         if ip4:protocol() == icmp.ICMP4.PROTOCOL then
-            self:icmp4(p)
-         else
-            self:protocol4_unreachable(p)
-         end
-      else
-         self:forward4(p)
-      end
-   else
-      self:reject_protocol(p)
-   end
-end
-
 function InboundDispatch:push ()
+   local eth_hdr4 = self.eth4:header()
    for _, input in ipairs(self.input) do
       while not link.empty(input) do
-         self:dispatch(link.receive(input))
+         local p = link.receive(input)
+         -- Prepend Ethernet pseudo header to please pf.match (we receive plain
+         -- IPv4 frames on the input port.)
+         p = packet.prepend(p, eth_hdr4, ethernet:sizeof())
+         self.p_box[0] = p
+         self:dispatch(p.data, p.length)
       end
    end
 end
