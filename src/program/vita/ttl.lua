c77f9249f1a8114da47562be153567fd3fe2307f
Max Rottenkolber <max@mr.gy>
2019-04-11 15:06:59 +0200

    vita.ttl: add DecrementHopLimit app

--- src/program/vita/ttl.lua
+++ src/program/vita/ttl.lua
@@ -4,6 +4,7 @@ module(...,package.seeall)
 
 local counter = require("core.counter")
 local ipv4 = require("lib.protocol.ipv4")
+local ipv6 = require("lib.protocol.ipv6")
 
 DecrementTTL = {
    name = "DecrementTTL",
@@ -28,6 +29,37 @@ function DecrementTTL:push ()
             link.transmit(output, p)
          elseif ip4 then
             link.transmit(time_exceeded, p)
+         else
+            packet.free(p)
+            counter.add(self.shm.protocol_errors)
+         end
+      end
+   end
+end
+
+DecrementHopLimit = {
+   name = "DecrementHopLimit",
+   shm = {
+      protocol_errors = {counter}
+   }
+}
+
+function DecrementHopLimit:new ()
+   return setmetatable({ip6 = ipv6:new({})}, {__index=DecrementHopLimit})
+end
+
+function DecrementHopLimit:push ()
+   local output = self.output.output
+   local hop_limit_exceeded = self.output.hop_limit_exceeded
+   for _, input in ipairs(self.input) do
+      while not link.empty(input) do
+         local p = link.receive(input)
+         local ip6 = self.ip6:new_from_mem(p.data, p.length)
+         if ip6 and ip6:hop_limit() > 0 then
+            ip6:hop_limit(ip6:hop_limit() - 1)
+            link.transmit(output, p)
+         elseif ip6 then
+            link.transmit(hop_limit_exceeded, p)
          else
             packet.free(p)
             counter.add(self.shm.protocol_errors)
